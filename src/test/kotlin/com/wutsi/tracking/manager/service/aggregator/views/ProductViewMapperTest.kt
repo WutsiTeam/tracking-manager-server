088f4cf687cc1241e5a9d1cb7621ef8d2603c4e1
wutsi <herve.tchepannou@wutsi.com>
2022-12-31 22:13:15 -0500

    fix tests

--- src/test/kotlin/com/wutsi/tracking/manager/service/aggregator/views/ProductViewMapperTest.kt
+++ src/test/kotlin/com/wutsi/tracking/manager/service/aggregator/views/ProductViewMapperTest.kt
@@ -4,19 +4,20 @@ import com.wutsi.tracking.manager.Fixtures
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.OffsetDateTime
+import java.time.ZoneId
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 internal class ProductViewMapperTest {
-    private val mapper = ProductViewMapper(LocalDate.now())
+    private val mapper = ProductViewMapper(LocalDate.now(ZoneId.of("UTC")))
 
     private val track = Fixtures.createTrackEntity(
         bot = false,
         page = ProductViewMapper.PAGE,
         event = ProductViewMapper.EVENT,
         productId = "123",
-        time = OffsetDateTime.now().toInstant().toEpochMilli(),
+        time = OffsetDateTime.now(ZoneId.of("UTC")).toInstant().toEpochMilli(),
     )
 
     @Test
